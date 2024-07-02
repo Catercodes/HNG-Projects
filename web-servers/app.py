4cc8496f51292eaf75bc5459f384ce8f409b7def
Catercodes <chrisadamsadewuni@gmail.com>
2024-07-02 22:51:32 +0100

    WebserverA

--- web-servers/app.py
+++ web-servers/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
 import requests
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
 
 # Load API keys from environment variables
-LOCATION_API_KEY = os.getenv('LOCATION_API_KEY', 'AIzaSyBAk-JHCSX1XWfzgedBFlamrZc3fOZjYYk')  # Your Google Geocoding API key
-WEATHER_API_KEY = os.getenv('WEATHER_API_KEY', '5e81005fb4770111451f7fe8f3ebdaae')           # Your OpenWeatherMap API key
+LOCATION_API_KEY = os.getenv('LOCATION_API_KEY')
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
 @app.route('/')
 def index():
@@ -19,37 +19,33 @@ def index():
 def get_location_and_weather():
     client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
 
+    # Example coordinates for testing
+    latitude, longitude = 40.730610, -73.935242
+
     # Get location data using Google Geocoding API
-    location_url = f'https://maps.googleapis.com/maps/api/geocode/json?key={LOCATION_API_KEY}&sensor=false&latlng=40.730610,-73.935242'
+    location_url = f'https://maps.googleapis.com/maps/api/geocode/json?key={LOCATION_API_KEY}&latlng={latitude},{longitude}'
     location_response = requests.get(location_url)
     location_data = location_response.json()
 
-    # Extract city and coordinates
     if location_data['status'] == 'OK':
         results = location_data['results'][0]
+        city = None
         for component in results['address_components']:
             if 'locality' in component['types']:
                 city = component['long_name']
                 break
-        else:
-            city = 'Unknown City'
-        
-        latitude = results['geometry']['location']['lat']
-        longitude = results['geometry']['location']['lng']
+        city = city or 'Unknown City'
     else:
         city = 'Unknown City'
-        latitude = 0.0
-        longitude = 0.0
+        latitude, longitude = 0.0, 0.0
 
     # Get weather data from OpenWeatherMap
     weather_url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={WEATHER_API_KEY}&units=metric'
     weather_response = requests.get(weather_url)
     weather_data = weather_response.json()
 
-    # Extract temperature
     temperature = weather_data.get('main', {}).get('temp', 'N/A')
 
-    # Create greeting message
     greeting = f"Hello, Mark!, the temperature is {temperature} degrees Celsius in {city}"
 
     return jsonify({
