244526789c457328213b75971e5bddab883caaef
Catercodes <chrisadamsadewuni@gmail.com>
2024-07-04 00:24:45 +0100

    web

--- web-servers/app.py
+++ web-servers/app.py
@@ -1,58 +1,58 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 import requests
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 app = Flask(__name__)
 
-# Load API keys from environment variables
-LOCATION_API_KEY = os.getenv('LOCATION_API_KEY')
-WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+# Free APIs for geolocation and weather data
+GEOLOCATION_API_URL = "http://ip-api.com/json/"
+WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
+WEATHER_API_KEY = "6eac1c3b94b30c0a0658661b8e02c2c9"  # Replace with your OpenWeatherMap API key
 
 @app.route('/')
 def index():
-    return "Welcome to my Flask API!"
+    return "Welcome to the GeoIP and Weather API. Use the /api/hello endpoint with the visitor_name parameter."
 
-@app.route('/api/location', methods=['GET'])
-def get_location_and_weather():
-    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
 
-    # Example coordinates for testing
-    latitude, longitude = 40.730610, -73.935242
+def get_geolocation(ip):
+    if ip == '127.0.0.1':
+        ip ='8.8.8.8'
 
-    # Get location data using Google Geocoding API
-    location_url = f'https://maps.googleapis.com/maps/api/geocode/json?key={LOCATION_API_KEY}&latlng={latitude},{longitude}'
-    location_response = requests.get(location_url)
-    location_data = location_response.json()
+    response = requests.get(GEOLOCATION_API_URL + ip)
+    return response.json()
 
-    if location_data['status'] == 'OK':
-        results = location_data['results'][0]
-        city = None
-        for component in results['address_components']:
-            if 'locality' in component['types']:
-                city = component['long_name']
-                break
-        city = city or 'Unknown City'
-    else:
-        city = 'Unknown City'
-        latitude, longitude = 0.0, 0.0
+def get_weather(city):
+    params = {
+        'q': city,
+        'appid': WEATHER_API_KEY,
+        'units': 'metric'  # Get temperature in Celsius
+    }
+    response = requests.get(WEATHER_API_URL, params=params)
+    return response.json()
 
-    # Get weather data from OpenWeatherMap
-    weather_url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={WEATHER_API_KEY}&units=metric'
-    weather_response = requests.get(weather_url)
-    weather_data = weather_response.json()
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    visitor_name = request.args.get('visitor_name', 'Unknown')
+    
+    # Get client's IP address
+    client_ip = request.remote_addr
 
-    temperature = weather_data.get('main', {}).get('temp', 'N/A')
+    # Get geolocation information
+    geo_info = get_geolocation(client_ip)
+    city = geo_info.get('city', 'Unknown')
 
-    greeting = f"Hello, Mark!, the temperature is {temperature} degrees Celsius in {city}"
+    # Get weather information
+    weather_info = get_weather(city)
+    temperature = weather_info['main']['temp'] if 'main' in weather_info else 'Unknown'
 
-    return jsonify({
+    greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
+
+    response = {
         "client_ip": client_ip,
         "location": city,
         "greeting": greeting
-    })
+    }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
